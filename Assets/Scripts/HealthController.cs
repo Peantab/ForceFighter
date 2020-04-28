9b806289eb23a3c0c622505fa7dc0b46f9b7fb4b
Peantab <peantab@gmail.com>
2020-04-28 22:51:33 +0200

    Efekt przyjmowania obrażeń.

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -3,14 +3,22 @@ using UnityEngine.UI;
 
 public class HealthController : MonoBehaviour
 {
+    public GameObject BloodSplash;
+
     private Text textbox;
     private int health;
+    private int splashCounter;
+    private const int splashCounterMax = 15;
+
+    private Image bloodSplashImage;
 
     // Start is called before the first frame update
     void Start()
     {
         textbox = gameObject.GetComponent<Text>();
         health = 100;
+
+        bloodSplashImage = BloodSplash.GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -30,9 +38,24 @@ public class HealthController : MonoBehaviour
         }
     }
 
+    private void FixedUpdate()
+    {
+        if (splashCounter > 0)
+        {
+            splashCounter--;
+            bloodSplashImage.color = new Color(bloodSplashImage.color.r, bloodSplashImage.color.g, bloodSplashImage.color.b, 1.0f * splashCounter/splashCounterMax);
+            if (splashCounter == 0) bloodSplashImage.enabled = false;
+            else bloodSplashImage.enabled = true;
+        }
+    }
+
     public void AcceptDamage(int damage)
     {
-        if (damage > 0) health -= damage;
+        if (damage > 0)
+        {
+            health -= damage;
+            splashCounter = splashCounterMax;
+        }
     }
 
     public bool IsAlive()
